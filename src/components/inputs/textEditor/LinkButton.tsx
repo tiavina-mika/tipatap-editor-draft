254ca9055e5ac4f948284aee92244a875f3ec2e5
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-10-30 10:53:26 +0300

    add button

--- src/components/inputs/textEditor/LinkButton.tsx
+++ src/components/inputs/textEditor/LinkButton.tsx
@@ -2,7 +2,14 @@
 /* @jsx jsx */
 /** @jsxImportSource @emotion/react */
 import { jsx } from "@emotion/react";
-import { IconButton } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton
+} from "@mui/material";
 import { Editor } from "@tiptap/react";
 import { Fragment, useState } from "react";
 
@@ -24,6 +31,23 @@ const LinkButton = ({ editor, className }: Props) => {
       <IconButton onClick={toggleLinkDialog} className={className}>
         <img alt="link" src="/icons/link.svg" />
       </IconButton>
+      <Dialog
+        open={openLinkDialog}
+        onClose={toggleLinkDialog}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>Cool</DialogContent>
+        <DialogActions>
+          <Button onClick={toggleLinkDialog}>Disagree</Button>
+          <Button onClick={toggleLinkDialog} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Fragment>
   );
 };
