73dacc17145ca0fd794884761b40687fceb893f8
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2024-04-12 13:47:38 +0300

    fix link

--- src/components/form/inputs/textEditor/MenuBar.tsx
+++ src/components/form/inputs/textEditor/MenuBar.tsx
@@ -19,8 +19,7 @@ import YoutubeDialog from "./YoutubeDialog";
 
 const classes = {
   menu: (theme: Theme) => ({
-    border: "1px solid " + theme.palette.grey[100],
-    borderLeft: "none" // because of sparkles button background
+    border: "1px solid " + theme.palette.grey[100]
   }),
   button: (isActive: boolean, split: boolean) => (theme: Theme) => ({
     borderRadius: 0,
@@ -292,6 +291,13 @@ const MenuBar = ({
         </IconButton>
 
         {/* youtube dialog */}
+        <LinkDialog
+          editor={editor}
+          open={openLinkDialog}
+          onClose={toggleLinkDialog}
+        />
+
+        {/* youtube dialog */}
         <YoutubeDialog
           editor={editor}
           open={openYoutubeDialog}
@@ -313,78 +319,6 @@ const MenuBar = ({
           anchorEl={headingAnchorEl}
           onClose={handleCloseHeadingMenu}
         />
-        {/* 
-        <button
-          onClick={() => editor.chain().focus().toggleBulletList().run()}
-          className={editor.isActive("bulletList") ? "is-active" : ""}
-        >
-          bullet list
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleCode().run()}
-          disabled={!editor.can().chain().focus().toggleCode().run()}
-          className={editor.isActive("code") ? "is-active" : ""}
-        >
-          code
-        </button>
-        <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
-          clear marks
-        </button>
-        <button onClick={() => editor.chain().focus().clearNodes().run()}>
-          clear nodes
-        </button>
-        <button
-          onClick={() => editor.chain().focus().setParagraph().run()}
-          className={editor.isActive("paragraph") ? "is-active" : ""}
-        >
-          paragraph
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-          className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
-        >
-          h1
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-          className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
-        >
-          h2
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-          className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
-        >
-          h3
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-          className={editor.isActive("heading", { level: 4 }) ? "is-active" : ""}
-        >
-          h4
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-          className={editor.isActive("heading", { level: 5 }) ? "is-active" : ""}
-        >
-          h5
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-          className={editor.isActive("heading", { level: 6 }) ? "is-active" : ""}
-        >
-          h6
-        </button>
-
-        
-        <button
-          onClick={() => editor.chain().focus().setColor("#958DF1").run()}
-          className={
-            editor.isActive("textStyle", { color: "#958DF1" }) ? "is-active" : ""
-          }
-        >
-          purple
-        </button> */}
       </div>
     </div>
   );
